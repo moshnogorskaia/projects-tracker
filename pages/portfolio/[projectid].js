1f5cb7cc6ff3d6478f3114de476ac36dcbd31bb7
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-12-04 19:32:39 +0100

    Extract dynamic path part

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,8 @@
+import { useRouter } from "next/router";
+
 function PortfolioProjectPage() {
+  const router = useRouter();
+  console.log(router.query.projectid);
   return (
     <div>
       <h1>Portfolio Project Page</h1>
