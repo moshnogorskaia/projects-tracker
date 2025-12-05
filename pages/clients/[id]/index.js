fa1ac1490eb619cdcd55e8f42341487c94098aa7
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-12-05 09:10:01 +0100

    Naviagte programmatically to the project

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,7 +3,20 @@ import { useRouter } from "next/router";
 function ClientProjectsPage({ params }) {
   const router = useRouter();
   console.log(router.query.id);
-  return <div>ClientPage</div>;
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+
+  return (
+    <div>
+      <h1>Client Projects Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
 }
 
 export default ClientProjectsPage;
